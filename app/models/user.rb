2f02eb7fcfac6b23e368c6f47e5b228d0a0fea30
Yoshidamouth <gomanosuke0507@gmail.com>
2024-06-30 21:41:32 +0900

    バリデーション・アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :affiliation_id, presence: true
+
+  belongs_to :affiliation
+  has_many :requests
+  has_many :comments
 end
