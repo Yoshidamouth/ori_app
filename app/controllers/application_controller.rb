ad5bb1a35115b1e08aed5638b67214e23d5603a6
Yoshidamouth <gomanosuke0507@gmail.com>
2024-07-01 21:47:15 +0900

    依頼一覧画面準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :login_required
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -8,12 +7,6 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
-
-  def login_required
-    unless user_signed_in?
-      redirect_to unauthenticated_root_path
     end
   end
 
