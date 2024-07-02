c268564e307542da94c6146efa3e169269ce0fdc
Yoshidamouth <gomanosuke0507@gmail.com>
2024-07-02 19:25:02 +0900

    コンフリクト解消

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -2,10 +2,10 @@ class Affiliation < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '本社' },
-    { id: 3, name: '東営業所' },
-    { id: 4, name: '西営業所' },
-    { id: 5, name: '南営業所' },
-    { id: 6, name: '北営業所' },
+    { id: 3, name: '東支店' },
+    { id: 4, name: '西支店' },
+    { id: 5, name: '南支店' },
+    { id: 6, name: '北支店' },
   ]
   include ActiveHash::Associations
   has_many :users
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root to: 'home#index'
+
+  authenticated :user do
+    # ログインしているユーザーのルート
+    root to: "requests#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    # ログインしていないユーザーのルート
+    root to: "home#index", as: :unauthenticated_root
+  end
+
   resources :home, only: :index
+  resources :requests, only: :index
+  get 'mypage', to: 'mypage#show', as: 'mypage'
+
 end
