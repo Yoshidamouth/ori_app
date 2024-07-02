617ba099fa6ea6c8e1057e1054425566edbc0ebd
Yoshidamouth <gomanosuke0507@gmail.com>
2024-07-02 18:38:00 +0900

    Revert "Merge pull request #4 from Yoshidamouth/revert-3-ユーザー管理機能"
    This reverts commit 3f6876fee74d5d5010d89fc718e4ce2ae30e0a09, reversing
    changes made to 2b1b070c3d6b68a59a0c858426992362c041656f.

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :affiliation
+
+  validates :name, presence: true
+  validates :affiliation_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"} 
+
+  # belongs_to :affiliation
+  has_many :requests
+  # has_many :comments
+end
