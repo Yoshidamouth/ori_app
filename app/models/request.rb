fe4a69e213b2c561e82df19349865410c157b862
Yoshidamouth <gomanosuke0507@gmail.com>
2024-07-05 00:21:42 +0900

    エラー分修正

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,21 +1,12 @@
 class Request < ApplicationRecord
-  validates :article_id, :quantity, :status, :user_id, :request_time, presence: true
-  validates :quantity, format: { with: /\A\d+\z/, message: "は半角数字で入力してください" },
+  validates :article_id, presence: { message: "を選択してください" }
+  validates :quantity, presence: { message: "を入力してください" },
+                       format: { with: /\A\d+\z/, message: "は半角数字で入力してください" },
                        numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
+  validates :status, :user_id, :request_time, presence: true
 
   has_many :comments
   belongs_to :user
   belongs_to :article
   belongs_to :response_user, class_name: 'User', optional: true
-
-  validate :custom_article_validation
-
-  private
-
-  def custom_article_validation
-    errors.delete(:article_id)
-    if article_id.blank?
-      errors.add(:article_id, "を選択してください")
-    end
-  end
 end
